db5b39a73577742418f4d529756a052c247e292f
Tuan Anh Tran <me@tuananh.org>
2022-11-01 14:44:35 +0700

    Update camaro.test.js

--- camaro.test.js
+++ camaro.test.js
@@ -2,6 +2,5 @@ const { transform } = require('camaro')
 
 test('camaro transform()', async () => {
   const result = await transform('<foo>bar</foo>', { foo: 'foo'})
-  console.log('result', result);
   expect(result).toEqual({foo: 'bar'})
 });
